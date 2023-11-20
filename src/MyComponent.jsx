26c300c61e897cb1799495a4cac06a6d23872bed
Evgenii Zakharov <eugene_zaxarov_1997@mail.ru>
2023-11-20 11:02:19 +0300

    start this

---
+++ src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+const MyComponent = (props) => {
+    const date = new Date();
+
+    useEffect(() => {
+        console.log(date);
+
+    }, []);
+    return (
+        <div> {date} </div>
+    );
+}
